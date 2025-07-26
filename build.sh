54275618fb76547bfdcb8a3b4dd6fd59f0d03b38
NerdyDad <nerdydad@nerdydad.me>
2025-07-26 13:05:15 -0500

    build.sh Line 8 - Put HUGO_VERSION in braces instead of parenthasis Line 12 - Removed readme.me because build failed at it's non-existence

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ main() {
     export TZ=America/Chicago
 
     # Install Hugo
-    echo "Installing Hugo v$(HUGO_VERSION)..."
+    echo "Installing Hugo v${HUGO_VERSION}..."
     curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
     tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
     cp hugo /opt/buildhome
-    rm LICENSE README.me hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+    rm LICENSE hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 
     # Verify installed versions
     echo "Verifying installations..."
